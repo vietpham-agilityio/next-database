b4e32611287ddf59f9d286dbe9b9b23040a988b1
Viet Pham <viet.pham@asnet.com.vn>
2024-05-10 10:55:43 +0700

    #10 Handling Errors and Not Found

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,14 +2,21 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
+// not found
+import { notFound } from 'next/navigation';
 
-    // fetch invoice by id and list customers
-    const [invoice, customers] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
-      ]);
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+
+  // fetch invoice by id and list customers
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
